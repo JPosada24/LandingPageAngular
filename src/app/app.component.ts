3dc96ac7b0501cb67aecfd204e2660cf15448bd2
Julian Posada <julian_2120@hotmail.com>
2024-01-18 21:39:02 -0500

    Proyecto Landing Page en Angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass, RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'landingPage';
+  menuOption: string = '';
+
+  onOption(menuOption: string) {
+    this.menuOption = menuOption;
+  }
 }
